4c7823b4ff434e8035368a90630069b89acb6e5e
AlexxxLy <alexxx.ax4@gmail.com>
2017-10-10 01:17:43 +0300

    Вроде готово! ))

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
+
 <?php
 
+$revertArray = ["first" => "olleH",
+                "wrong" => "dlroW", "dlroW", "hsiniF"];
 
+krsort($revertArray);
+array_shift($revertArray);
+$array2 = array_unique($revertArray);
+arsort($array2);
+
+foreach ($array2 as $item => $value)
+{
+   $array2[$item] = strrev($value);
+}
+
+print_r($array2);
 
 ?>
+ 
