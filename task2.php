a9a53b6002d877ec3ef68c967c55e55e4318a832
AlexxxLy <alexxx.ax4@gmail.com>
2017-10-15 21:14:42 +0300

    Creat class

--- task2.php
+++ task2.php
@@ -1,7 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: AlexPHP
- * Date: 13.10.2017
- * Time: 22:12
+
+ class Animal{
+   public $name;
+   public $speak;
+   public $can;
+
+  public function printClass($name,$speak,$can)
+  {
+//      $this->name=$name;
+//      $this->speak=$speak;
+//      $this->can=$can;
+      echo " **** ";
+      echo "My name is " .$name." ";
+      echo "I cant speak ". $speak." ";
+      echo "I cant ".$can." ";
+
+  }
+
+}
+ class Gus extends Animal{};
+ $gus = new Gus;
+ $gus->name='Sheptun';
+ $gus->speak='Ga-ga-ga';
+ $gus->can='Fly';
+ $gus->printClass($name,$speak,$can);
+
+ /*class Dog extends Animal{
+
+//     private $tail;
+     private $breed;
+     public function __construct($name, $speak, $can)
+     {
+         parent::__construct($name, $speak, $can);
+     }
+}
+$dog = new Dog('Chaky','Gav-gav','run','Labrador');
  */
+?>
