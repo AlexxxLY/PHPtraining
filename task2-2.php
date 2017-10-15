d02e0371063fc0880ad9c0ab7a7ea38e8f2d122b
AlexxxLy <alexxx.ax4@gmail.com>
2017-10-15 23:01:15 +0300

    Ready!!!!

--- task2-2.php
+++ task2-2.php
@@ -1,3 +1,55 @@
 <?php
 
+ class All{
+     public $name;
+     public $vol;
+     public $species;
+
+     public function Volume($vol)
+     {
+         $this->vol=$vol;
+         echo " I speak ".$vol." ";
+     }
+
+ }
+ class Gus extends All {
+    public $species=' Gus ';
+    public function Fly(){
+        echo " I can Fly! ";
+    }
+ }
+
+ $gus = new Gus;
+ $gus->name=' Sheptun ';
+ $gus->vol=' Ga-ga-ga ';
+
+ class Dog extends All {
+
+     public $species=' Dog ';
+
+     public function tail(){
+         echo " I have a Tail ";
+     }
+     public function Run(){
+        echo " I can RUN! ";
+    }
+}
+
+class labrador extends Dog{
+     public $breed=' Labrador ';
+}
+$dog = new labrador;
+$dog->name=' Chaky ';
+$dog->vol=' Gav-gav ';
+
+echo " I m ".$gus->species.". My name is ".$gus->name;
+     $gus->Volume($gus->vol);
+     $gus->Fly();
+     echo"****";
+echo " I m ".$dog->species.". My name is ".$dog->name.
+     ". My breed is ".$dog->breed;
+     $dog->Volume($dog->vol);
+     $dog->Run();
+     $dog->tail();
+     echo"****";
 ?>
